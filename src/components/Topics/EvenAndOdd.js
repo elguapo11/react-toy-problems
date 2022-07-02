05801939a2641367c70703471e59424035474aec
Benny Mendoza <bennybc9387@gmail.com>
2022-07-02 11:32:34 -0600

    error being thrown

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function EvenAndOdd() {
+    const [userInput, setUserInput] = useState('');
+    const [ evens, setEvens ] = useState([]);
+  const [odds, setOdds] = useState([]);
+
+
+  function solve(input) {
+    const results = {
+      evens: [],
+      odds: [],
+    }
+    for (let val of input.split(',').map(v => parseInt(v))) {
+      if (val % 2 === 0) {
+        results.evens.push(val);
+      } else {
+        results.odds.push(val);
+      }
+    }
+
+    setEvens(results.evens);
+    setOdds(results.odds);
+  }
+
   return (
-    <p> EvenAndOdds</p>
+    <div className="puzzleBox evenAndOddPB">
+      <h4>Evens and Odds</h4>
+      <input className="inputLine" onChange={e => setUserInput(e.target.value)}/>
+      <button className="confirmationButton" onClick={() => solve(userInput)}>Split</button>
+      <span className="resultsBox">Evens: {JSON.stringify(evens)}</span>
+      <span className="resultsBox">Odds: {JSON.stringify(odds)}</span>
+    </div>
   )
 }
